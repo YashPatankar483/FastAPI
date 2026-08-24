b13547b59201de8cdff8dd1420158d871cc3bc15
Yash Patankar <yashpatankar483@gmail.com>
2026-08-24 23:11:06 +0530

    added a sort patients feature using the query parameter

--- doctors_cases.py
+++ doctors_cases.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()
@@ -34,4 +34,27 @@ def patient_info(patient_id : str = Path(..., description="This path param ident
 
             else:
                  raise HTTPException(status_code=404, detail="Patient not found")
-            
+
+
+
+@app.get("/sort")
+def sorted_list(sort_by: str = Query(..., description="height_cm, weight_kg, bmi, heart_rate_bpm"), order: str = Query("asc", description="asc or desc")):
+
+     valid_fields = ["height", "weight", "bmi", "heart_rate"]
+
+     if sort_by not in valid_fields:
+          raise HTTPException(status_code=400, detail=f"Invalid field, select from {valid_fields}")
+
+     if order not in ["asc", "desc"]:
+          raise HTTPException(status_code=400, detail=f"Invalid field, select from {["asc", "desc"]}")
+
+     with open("data.json", "r") as f:
+          data = json.load(f)
+
+          patients = data["patients"]
+
+          sort_order = True if order=="desc" else False
+
+          sorted_data = sorted(patients, key = lambda x: x.get(sort_by, 0), reverse=sort_order)
+
+          return sorted_data
